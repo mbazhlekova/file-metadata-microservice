4aaaf3e60d6c96c0ee22da61196a506238b2ab36
mbazhlekova <maria.bazhlekova@gmail.com>
2017-06-26 21:36:24 -0500

    added post to handle file upload

--- routes/index.js
+++ routes/index.js
@@ -1,14 +1,20 @@
 import express from 'express';
 import multer from 'multer';
-const upload = multer({ dest: '/uploads'});
+import path from 'path';
+//  import bodyParser from 'body-parser';
 
 const router = express.Router();
 
 /* GET index page. */
 router.get('/', (req, res, next) => {
-  res.render('index', {
-    title: 'Express'
-  });
+  res.render('index');
+});
+
+router.post('/', multer({
+  dest: path.join(__dirname, '/uploads/')
+}).single('upl'), (req, res) => {
+  console.log(req.body);
+  console.log(req.file);
 });
 
 export default router;
